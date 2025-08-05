7b0c399e38be4c7bb549199c005ff02197218207
Reatha Thorn <reathathorn36@gmail.com>
2025-08-05 14:23:46 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create shapes
+        Square square = new Square("Red", 4);
+        Rectangle rectangle = new Rectangle("Blue", 5, 3);
+        Circle circle = new Circle("Green", 2.5);
+
+        // Store them in a list
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        // Display color and area for each shape
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():F2}");
+            Console.WriteLine();
+        }
+    }
+}
